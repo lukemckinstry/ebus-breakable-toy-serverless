4ac63d59ed95de36824f0894c7f1de641797e33a
Luke McKinstry <lukemckinstry@gmail.com>
2022-04-19 15:13:46 -0400

    first test for ingest process script

--- transit/tests.py
+++ transit/tests.py
@@ -2,13 +2,28 @@ from django.urls import reverse
 from rest_framework import status
 from .models import Agency
 from rest_framework.test import APITestCase
+from django.test import TestCase
+from gtfs import loadfeeds
 
+class LoadfeedsTests(TestCase):
+
+    def test_loadfeeds(self):
+        """
+        Ensure ingest of known gtfs file produces expected values for agency url 
+        and number of routes in database
+        """
+        loadfeeds.main('cata')
+        qs = Agency.objects.filter(agency_name='CATA')
+        num_routes = qs[0].route_set.count()
+        self.assertIs(num_routes, 9)
+        self.assertEqual(qs[0].agency_url, 'http://www.catabus.com')
 
 class AgencyTests(APITestCase):
     def test_create_account(self):
         """
         Ensure we can create a new agency object.
         """
+        print("Hi!")
         url = reverse('agency_list')
         data = {
             'name': 'TestAgency',

a7104c7b5eecd6352b1444b03b6b11f941700657
Luke McKinstry <lukemckinstry@gmail.com>
2022-04-19 09:52:55 -0400

    create agency list endpoint with drf view

--- transit/urls.py
+++ transit/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('agency/', views.agency_list),
     path('', views.index, name='index'),
 ]
--- transit/views.py
+++ transit/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
+from .models import Agency
+
+from rest_framework.response import Response
+from .serializers import *
+from rest_framework.decorators import api_view
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the routes index.")
+
+@api_view(['GET'])
+def agency_list(request):
+    """   List agencies """
+
+    if request.method == 'GET':
+        data = []
+        nextPage = 1
+        previousPage = 1
+        qs = Agency.objects.all().order_by('created')[:30]
+        serializer = AgencySerializer(qs,context={'request': request} ,many=True)
+        return Response({'data': serializer.data})

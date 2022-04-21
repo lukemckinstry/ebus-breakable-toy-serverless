9b40fb0c58857d3e0fade8b487813e7b5d404548
Luke McKinstry <lukemckinstry@gmail.com>
2022-04-21 10:48:37 -0400

    add tests for route detail endpoint

--- transit/tests.py
+++ transit/tests.py
@@ -30,6 +30,36 @@ def create_agency(self):
     response = self.client.post(url, data, format='json')
     return response
 
+def create_route(self, agency):
+    test_agency_uuid = agency.id
+    test_agency_id = agency.agency_id
+    url = reverse('route_list', args=[test_agency_id,])
+    data = {
+        'route_id':'999',
+        'agency_id':test_agency_uuid,
+		'route_short_name':'',
+		'route_long_name':'',
+		'route_desc':'Test desc',
+		'route_type':'3',
+		'route_url':'',
+		'route_color':'',
+		'route_text_color':'',
+		'route_sort_order':'',
+		'trips_monday':'0',
+		'trips_tuesday':'0',
+		'trips_wednesday':'0',
+		'trips_thursday':'0',
+		'trips_friday':'0',
+		'trips_saturday':'0',
+		'trips_sunday':'0',
+        'zev_charging_infrastrucutre':'False',
+        'zev_notes':'',
+        'pct_zev_service':'0.0',
+        'num_zev':'0',
+    }
+    response = self.client.post(url, data, format='json')
+    return response
+
 class AgencyTests(APITestCase):
     def test_create_agency(self):
         """
@@ -46,14 +76,25 @@ class RouteTests(APITestCase):
         Ensure we can create a new route object.
         """
         _ = create_agency(self)
-        test_agency_id = Agency.objects.get().id
-        test_agency_name = Agency.objects.get().agency_name
-        url = reverse('route_list', args=[test_agency_id,])
+        agency = Agency.objects.get()
+        response = create_route(self, agency)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Route.objects.count(), 1)
+        self.assertEqual(Route.objects.get().route_id, '999')
 
+    def test_update_route(self):
+        """
+        Ensure we can update a route object.
+        """
+        _ = create_agency(self)
+        agency = Agency.objects.get()
+        _ = create_route(self, agency)
+        route = Route.objects.get()
+        test_route_id = route.id
+        url = reverse('route_detail', args=[test_route_id,])
         data = {
-            'route_id':'999',
-			'agency_name':test_agency_name,
-            'agency_id':test_agency_id,
+            'id': test_route_id,
+            'route_id':'888',
 			'route_short_name':'',
 			'route_long_name':'',
 			'route_desc':'Test desc',
@@ -74,7 +115,7 @@ class RouteTests(APITestCase):
             'pct_zev_service':'0.0',
             'num_zev':'0',
         }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Route.objects.count(), 1)
-        self.assertEqual(Route.objects.get().route_id, '999')
+        self.assertEqual(Route.objects.get().route_id, '888')

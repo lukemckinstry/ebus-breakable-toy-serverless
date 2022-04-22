fafeb7193030a3b1c7adb5d7eb877a766e60de7d
Luke McKinstry <lukemckinstry@gmail.com>
2022-04-22 09:35:31 -0400

    add arg to gtfs script to download gtfs sample

--- transit/management/commands/loadfeeds.py
+++ transit/management/commands/loadfeeds.py
@@ -1,8 +1,8 @@
 import os, sys
 import glob
 import csv
+import urllib.request
 import zipfile
-import pickle
 import logging
 
 from django.contrib.gis.geos import LineString, MultiLineString
@@ -12,14 +12,26 @@ from transit.models import Agency, Route
 
 logging.basicConfig()
 LOG = logging.getLogger()
-LOG.setLevel(logging.INFO)
 
 
-def get_gtfs_agency_zips():
+def get_gtfs_agency_zips(sample):
     pwd = os.path.dirname(os.path.abspath(__file__))
-    print(pwd)
+    LOG.debug(pwd)
+    if sample:
+        LOG.info("retrieve sample gtfs file {}".format(sample))
+        try:
+            resultFilePath, responseHeaders = urllib.request.urlretrieve(
+                sample, pwd + "/sample.zip"
+            )
+        except:
+            LOG.error("unable to download file from url: {}".format(sample))
+            return
+        print(responseHeaders)
+        LOG.info("saved sample gtfs file to {}".format(resultFilePath))
+
     files = glob.glob(pwd + "/*.zip")
-    print(files)
+    LOG.info(files)
+
     return files
 
 
@@ -130,20 +142,20 @@ def read_write_gtfs(dd, ziplabel):
         agency_id = agency["agency_id"]
         agency_uuid = agency["uuid"]
 
-        print(agency_id, agency_uuid)
+        LOG.debug("agency_id: {} \t agency_uuid: {}".format(agency_id, agency_uuid))
 
         if "agency_id" in next(iter(dd["routes"]), None):
-            # print( 'agency_id is in routes' )
+            LOG.debug("agency_id is in routes")
             uu_routes = list(
                 set(
                     [t["route_id"] for t in dd["routes"] if t["agency_id"] == agency_id]
                 )
             )
         else:
-            # print( 'agency_id not in routes' )
+            LOG.debug("agency_id not in routes")
             uu_routes = list(set([t["route_id"] for t in dd["routes"]]))
 
-        print("ready to write # route: ", len(uu_routes))
+        LOG.info("ready to write # route: {}".format(len(uu_routes)))
 
         for route in uu_routes:
 
@@ -238,7 +250,7 @@ def read_write_gtfs(dd, ziplabel):
             for k in cal_object:
                 obj[k] = cal_object[k]
 
-            print("bad service_ids per route ", len(bad_service_ids_route))
+            LOG.info("bad service_ids per route {}".format(len(bad_service_ids_route)))
 
             for bsid in bad_service_ids_route:
                 if bsid not in bad_service_ids:
@@ -255,13 +267,13 @@ def read_write_gtfs(dd, ziplabel):
                 pass
 
     uu_service_ids = list(set([t["service_id"] for t in dd["trips"]]))
-    print("the agency has ", len(uu_service_ids), " unique service ids  ")
+    LOG.info("the agency has {} unique service ids".format(len(uu_service_ids)))
 
     uu_trips = list(set([t["trip_id"] for t in dd["trips"]]))
-    print("the agency has ", len(uu_trips), " unique trips ")
+    LOG.info("the agency has {} unique trips ".format(len(uu_trips)))
 
     uu_shapes = list(set([t["shape_id"] for t in dd["shapes"]]))
-    print("the agency has ", len(uu_shapes), " unique shapes ")
+    LOG.info("the agency has {} unique shapes ".format(len(uu_shapes)))
 
     return_obj["uu_service_ids"] = len(uu_service_ids)
     return_obj["uu_trips"] = len(uu_trips)
@@ -284,7 +296,7 @@ def check_route_types(dd):
             route_types[t["route_type"]] = 0
         else:
             route_types[t["route_type"]] += 1
-    print("route types ", route_types)
+    LOG.debug("route types ", route_types)
 
 
 def process_zip(az):
@@ -335,14 +347,38 @@ def iter_agencies(agency_zips):
 
 
 class Command(BaseCommand):
-    help = "Closes the specified poll for voting"
+    help = "Loads data from gtfs transit feed files into the database"
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument(
+            "--debug",
+            action="store_true",
+            help="Set log level to debug",
+        )
+
+        parser.add_argument(
+            "--sample",
+            nargs="?",
+            default=None,
+            const="https://storage.googleapis.com/storage/v1/b/mdb-latest/o/us-pennsylvania-centre-county-transit-authority-cata-gtfs-1236.zip?alt=media",
+            help="Download a sample gtfs file (option to provide a download url)",
+        )
 
     def handle(self, *args, **options):
+
+        if options["debug"]:
+            LOG.setLevel(logging.DEBUG)
+        else:
+            LOG.setLevel(logging.INFO)
         LOG.info("Going to gather gtfs feed zips")
-        gtfs_agency_zips = get_gtfs_agency_zips()
+        gtfs_agency_zips = get_gtfs_agency_zips(options["sample"])
+        if not gtfs_agency_zips:
+            LOG.info(
+                "no gtfs files found in {}, rerun script with --sample to download a file".format(
+                    os.path.dirname(os.path.abspath(__file__))
+                )
+            )
+            return
         LOG.info("zips: %s", gtfs_agency_zips)
         iter_agencies(gtfs_agency_zips)
 

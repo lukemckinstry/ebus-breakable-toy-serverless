344fe824184aa887d4d2b35ab54560439b87ab82
Luke McKinstry <lukemckinstry@gmail.com>
2022-04-28 09:07:48 -0400

    new url name for tile server endpoint

--- transit/urls.py
+++ transit/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("agency/", agency_list, name="agency-list"),
+    path("agency/<str:pk>/", agency_detail, name="agency-detail"),
+    path("agency/<str:agency_pk>/route", route_list, name="route-list"),
+    path("route/tiles", mvt_view_factory(Route)),
+    path("route/<str:pk>/", route_detail, name="route-detail"),
+    path("route/", route_create, name="route-create"),
+    path("basicmap/", views.basic_map),
+    path("", views.index, name="index"),
 ]

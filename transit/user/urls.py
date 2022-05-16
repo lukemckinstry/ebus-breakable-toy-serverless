4026ce6458f0994ffaebbf7ab3b5ab28400feea1
Luke McKinstry <lukemckinstry@gmail.com>
2022-05-16 09:58:05 -0400

    registration and login logic

--- transit/user/urls.py
+++ transit/user/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 from .views import UserViewSet
 
-user_list = UserViewSet.as_view({"get": "list", "post": "create"})
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+user_list = UserViewSet.as_view({"get": "list"})
+user_create = UserViewSet.as_view({"post": "create"})
 user_detail = UserViewSet.as_view(
     {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
 )
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register", user_create, name="user-register"),
     path("<str:pk>/", user_detail, name="user-detail"),
-    path("", user_list, name="agency-user"),
 ]
--- transit/user/views.py
+++ transit/user/views.py
@@ -1,11 +1,32 @@
 from .serializers import UserSerializer
 from .models import User
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import filters
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserViewSet(viewsets.ModelViewSet):
     http_method_names = ["get", "post"]
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        print("user create views")
+        serializer = self.get_serializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        res = {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
+
+        return Response(
+            {
+                "refresh": res["refresh"],
+                "token": res["access"],
+            },
+            status=status.HTTP_201_CREATED,
+        )

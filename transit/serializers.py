3376c29c17284f4baaeb2021a49ad1de71ec81a7
Luke McKinstry <lukemckinstry@gmail.com>
2023-06-20 15:52:07 -0400

    add endpoints to manage geometry for routes

--- transit/serializers.py
+++ transit/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework_gis.serializers import GeoFeatureModelSerializer
+
 from .models import Route, Agency
 
 
@@ -50,4 +52,37 @@ class AgencySerializer(serializers.ModelSerializer):
             "agency_email",
             "num_vehicles",
             "num_zero_emission_vehicles",
+        ]
+
+class RouteBatchSerializer(GeoFeatureModelSerializer):
+    agency = serializers.PrimaryKeyRelatedField(queryset=Agency.objects.all())
+
+    class Meta:
+        model = Route
+        geo_field = "geometry"
+        fields = [
+            "id",
+            "route_id",
+            "agency",
+            "route_short_name",
+            "route_long_name",
+            "route_desc",
+            "route_type",
+            "route_url",
+            "route_color",
+            "route_text_color",
+            "route_sort_order",
+            "geometry",
+            "route_distance",
+            "trips_monday",
+            "trips_tuesday",
+            "trips_wednesday",
+            "trips_thursday",
+            "trips_friday",
+            "trips_saturday",
+            "trips_sunday",
+            "zev_charging_infrastructure",
+            "zev_notes",
+            "pct_zev_service",
+            "num_zev",
         ]
--- transit/views.py
+++ transit/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from .serializers import AgencySerializer, RouteSerializer
+from .serializers import AgencySerializer, RouteSerializer, RouteBatchSerializer
 from .models import Agency, Route
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+
 
 
 def index(request):
@@ -19,7 +21,7 @@ def basic_map(request):
 
 
 def route_bbox(request, pk):
-    r = Route.objects.get(id=pk).mpoly.extent
+    r = Route.objects.get(id=pk).geometry.extent
     return JsonResponse({"bbox": r})
 
 
@@ -48,3 +50,42 @@ class RouteViewSet(viewsets.ModelViewSet):
             return Route.objects.all()  # all other requests
         queryset = Route.objects.filter(agency=agency)
         return queryset
+    
+
+class RouteBatchViewSet(viewsets.ModelViewSet):
+    """
+    Viewset for the batch upload process to automatically provide the `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+
+    serializer_class = RouteBatchSerializer
+    ## ToDo: Add permissions
+    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        agency = self.kwargs.get("agency_pk")  # supports get request by agency endpoint
+        if not agency:
+            return Route.objects.all()  # all other requests
+        queryset = Route.objects.filter(agency=agency)
+        return queryset
+    
+    def create(self, request, agency_pk):
+        route_copy = {**request.data}
+        route_copy["properties"] = {**route_copy["properties"], "agency": agency_pk}
+        serializer = self.get_serializer_class()
+        route_to_post = serializer(data=route_copy)
+        if route_to_post.is_valid():
+            route_to_post.save()
+            return Response(route_to_post.data, status=status.HTTP_201_CREATED)
+        return Response(route_to_post.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk):
+        route = Route.objects.get(id=pk) 
+        route_copy = {**request.data}
+        route_copy["properties"] = {**route_copy["properties"], "route": route.id, "agency": route.agency.id}
+        serializer = self.get_serializer_class()
+        route_to_update = serializer(route, data=route_copy)
+        if route_to_update.is_valid():
+            route_to_update.save()
+            return Response(route_to_update.data, status=status.HTTP_200_OK)
+        return Response(route_to_update.errors, status=status.HTTP_400_BAD_REQUEST)
